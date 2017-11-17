1853f0290126192d79073be7d664998cefaf4365
i.dobrobaba <dobrobaba.igor@omega-auto.biz>
2017-11-17 16:01:12 +0200

    new AccumSelection.cs test

---
+++ ToolsQA/TestsCases/SelectionAccum.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System.Collections.Generic;
+using ToolsQA.pages;
+
+namespace ToolsQA.TestsCases
+{
+    [TestFixture]
+    class SelectionAccum : BaseTest
+    {
+        private readonly AccumSelection accumSelection;
+
+        public SelectionAccum() : base()
+        {
+            accumSelection = new AccumSelection(_browser);
+        }
+
+        [OneTimeSetUp]
+        public void HideAdvancedSearch()
+        {
+            WaitForElementID("sidebarCategoryTires");
+            tiresSelection.OpenSelectionTires();
+            tiresSelection.ChatbarHide();
+        }
+
+        [TearDown]
+        public void ClearSearchParametrs()
+        {
+            _browser.WaitForAngular();
+            var removeLables = _browser.FindElements(By.XPath(".//*[@id='menu']/object/li[1]/span"));
+            for (var i = 0; i < removeLables.Count; i++)
+            {
+                removeLables[i].Click();
+            }
+
+        }
---
+++ ToolsQA/pages/AccumSelection.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+namespace ToolsQA.pages
+{
+    class AccumSelection
+    {
+        private IWebDriver _driver;
+
+        public AccumSelection(IWebDriver driver)
+        {
+            _driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.Id, Using = "sidebarCategoryAccum")]
+        [CacheLookup]
+        private IWebElement ButtonCategoryAccum { get; set; }
+
+        [FindsBy(How = How.Id, Using = "advancedsearchcategory")]
+        [CacheLookup]
+        private IWebElement ButtonEnterAdvancedSearch { get; set; }
+
+        [FindsBy(How = How.XPath, Using = ".//*[@id='chatbar']/div/div/aside/div/section[1]/accordion/div/h2/i")]
+        [CacheLookup]
+        private IWebElement ClickChatbar { get; set; }
+
+
+        public void OpenSelectionAccum()
+        {
+            ButtonCategoryAccum.Click();
+        }
+
+        public void ClickOnButtonApplyInAdvancedSearch()
+        {
+            ButtonEnterAdvancedSearch.Click();
+        }
+
+        public void ChatbarHide()
+        {
+            ClickChatbar.Click();
+        }
+       
+        }
+}
