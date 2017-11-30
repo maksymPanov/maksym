ea8aa5e463d6382647996da86aee893f4d4bad7a
i.dobrobaba <dobrobaba.igor@omega-auto.biz>
2017-11-30 16:24:31 +0200

    add new technicalFluidSelection tests

---
+++ ToolsQA/TestsCases/SelectionTechnicalFluid.cs
@@ -0,0 +1,126 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System.Collections.Generic;
+using ToolsQA.pages;
+
+namespace ToolsQA.TestsCases
+{
+    [TestFixture]
+    class SelectionTechnicalFluid : BaseTest
+    {
+        private readonly TechnicalFluidSelection technicalFluidSelection;
+
+        public SelectionTechnicalFluid() : base()
+        {
+            technicalFluidSelection = new TechnicalFluidSelection(_browser);
+        }
+
+        [OneTimeSetUp]
+        public void HideAdvancedSearch()
+        {
+            WaitForElementID("sidebarCategoryOil");
+            technicalFluidSelection.OpenSelectionOIL();
+            technicalFluidSelection.OpenSelectionTechnicalFluid();
+            technicalFluidSelection.ChatbarHide();
+        }
+
+        [TearDown]
+        public void ClearSearchParametrs()
+        {
+            _browser.WaitForAngular();
+            var removeLables = _browser.FindElements(By.XPath(".//*[@id='menu']/object/li[1]/span"));
+            for (var i = 0; i < removeLables.Count; i++)
+            {
+                removeLables[i].Click();
+            }
+        }
+
+        [Test]
+        public void TLAppointmentResultMustBeSuccess()
+        {
+
+            WaitForElementXpath(".//*[@id='menu']/li[2]/span");
+            technicalFluidSelection.OpenTFPurposeDdl();
+            technicalFluidSelection.ChooseTFPurpose();
+            technicalFluidSelection.ClickOnButtonApplyInAdvancedSearch();
+            var SearchResultTires = _browser.FindElement(By.LinkText("4802617334"));
+            Assert.IsTrue(SearchResultTires.Displayed);
+
+        }
+
+        [Test]
+        public void TLColorResultMustBeSuccess()
+        {
+
+            WaitForElementXpath(".//*[@id='menu']/li[2]/span");
+            technicalFluidSelection.OpenTFColorDdl();
+            technicalFluidSelection.ChooseTFColor();
+            technicalFluidSelection.ClickOnButtonApplyInAdvancedSearch();
+            var SearchResultTires = _browser.FindElement(By.LinkText("26580"));
+            Assert.IsTrue(SearchResultTires.Displayed);
+
+        }
+
+        [Test]
+        public void TFSeasonalityResultMustBeSuccess()
+        {
+
+            WaitForElementXpath(".//*[@id='menu']/li[2]/span");
+            technicalFluidSelection.OpenTFSeasonalityDdl();
+            technicalFluidSelection.ChooseTFSeasonality();
+            technicalFluidSelection.ClickOnButtonApplyInAdvancedSearch();
+            var SearchResultTires = _browser.FindElement(By.LinkText("5363"));
+            Assert.IsTrue(SearchResultTires.Displayed);
+
+        }
+
+        [Test]
+        public void SpecOEMResultMustBeSuccess()
+        {
+
+            WaitForElementXpath(".//*[@id='menu']/li[2]/span");
+            technicalFluidSelection.OpenTFSpecOEMyDdl();
+            technicalFluidSelection.ChooseTFSpecOEM();
+            technicalFluidSelection.ClickOnButtonApplyInAdvancedSearch();
+            var SearchResultTires = _browser.FindElement(By.LinkText("12710"));
+            Assert.IsTrue(SearchResultTires.Displayed);
+
+        }
+
+        [Test]
+        public void TFScopeResultMustBeSuccess()
+        {
+
+            WaitForElementXpath(".//*[@id='menu']/li[2]/span");
+            technicalFluidSelection.OpenTFScopeDdl();
+            technicalFluidSelection.ChooseTFScope();
+            technicalFluidSelection.ClickOnButtonApplyInAdvancedSearch();
+            var SearchResultTires = _browser.FindElement(By.LinkText("4102816901"));
+            Assert.IsTrue(SearchResultTires.Displayed);
+
+        }
+
+        [Test]
+        public void TiresMustHavePrices()
+        {
+            WaitForElementXpath(".//*[@id='menu']/li[2]/span");
+            var priceText = _browser.FindElement(By.CssSelector("td:nth-child(8) > span")).Text;
+            double number = 0;
+            Assert.DoesNotThrow(() => number = double.Parse(priceText.Replace(".", ",")));
+            Assert.Greater(number, 0);
+
+        }
+
+        [Test]
+        public void TiresMustHaveRests()
+        {
+            WaitForElementXpath(".//*[@id='menu']/li[2]/span");
+            var restsText = _browser.FindElement(By.CssSelector(".hidden-sm.hidden-xs")).Text;
+            if (restsText.Trim().Contains("-"))
+            {
+                Assert.Fail();
+            }
+        }
+
+    }
+}
---
+++ ToolsQA/pages/TechnicalFluidSelection.cs
@@ -0,0 +1,142 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+namespace ToolsQA.pages
+{
+    class TechnicalFluidSelection
+    {
+        private IWebDriver _driver;
+
+        public TechnicalFluidSelection(IWebDriver driver)
+        {
+            _driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.Id, Using = "sidebarCategoryOil")]
+        [CacheLookup]
+        private IWebElement ButtonCategoryOil { get; set; }
+
+        [FindsBy(How = How.Id, Using = "sidebarCategoryTechnicalFluid")]
+        [CacheLookup]
+        private IWebElement ButtonCategoryTechnicalFluid { get; set; }
+
+        [FindsBy(How = How.Id, Using = "advancedsearchcategory")]
+        [CacheLookup]
+        private IWebElement ButtonEnterAdvancedSearch { get; set; }
+
+        [FindsBy(How = How.XPath, Using = ".//*[@id='chatbar']/div/div/aside/div/section[1]/accordion/div/h2/i")]
+        [CacheLookup]
+        private IWebElement ClickChatbar { get; set; }
+
+        [FindsBy(How = How.Id, Using = "asideTFPurpose")]
+        [CacheLookup]
+        private IWebElement ButtonAsideTFPurpose { get; set; }
+
+        [FindsBy(How = How.XPath, Using = ".//*[@class='checkBoxContainer']//*[contains(text(),'Жидкость тормозная')]")]
+        [CacheLookup]
+        private IWebElement ParametrAsideChooseTFPurpose { get; set; }
+
+        [FindsBy(How = How.Id, Using = "asideTFColor")]
+        [CacheLookup]
+        private IWebElement ButtonAsideTFColor { get; set; }
+
+        [FindsBy(How = How.XPath, Using = ".//*[@class='checkBoxContainer']//*[contains(text(),'Зеленый')]")]
+        [CacheLookup]
+        private IWebElement ParametrAsideChooseTFColor { get; set; }
+
+        [FindsBy(How = How.Id, Using = "asideTFSeasonality")]
+        [CacheLookup]
+        private IWebElement ButtonAsideTFSeasonality { get; set; }
+
+        [FindsBy(How = How.XPath, Using = ".//*[@class='checkBoxContainer']//*[contains(text(),'Зимний')]")]
+        [CacheLookup]
+        private IWebElement ParametrAsideChooseTFSeasonality { get; set; }
+
+        [FindsBy(How = How.Id, Using = "asideTFSpecOEM")]
+        [CacheLookup]
+        private IWebElement ButtonAsideTFSpecOEM { get; set; }
+
+        [FindsBy(How = How.XPath, Using = ".//*[@class='checkBoxContainer']//*[contains(text(),'G12')]")]
+        [CacheLookup]
+        private IWebElement ParametrAsideChooseTFSpecOEM { get; set; }
+
+        [FindsBy(How = How.Id, Using = "asideTFScope")]
+        [CacheLookup]
+        private IWebElement ButtonAsideTFScope { get; set; }
+
+        [FindsBy(How = How.XPath, Using = ".//*[@class='checkBoxContainer']//*[contains(text(),'0,1')]")]
+        [CacheLookup]
+        private IWebElement ParametrAsideChooseTFScope { get; set; }
+
+        public void OpenSelectionOIL()
+        {
+            ButtonCategoryOil.Click();
+        }
+
+        public void OpenSelectionTechnicalFluid()
+        {
+            ButtonCategoryTechnicalFluid.Click();
+        }
+
+        public void ClickOnButtonApplyInAdvancedSearch()
+        {
+            ButtonEnterAdvancedSearch.Click();
+        }
+
+        public void ChatbarHide()
+        {
+            ClickChatbar.Click();
+        }
+
+        public void OpenTFPurposeDdl()
+        {
+            ButtonAsideTFPurpose.Click();
+        }
+
+        public void ChooseTFPurpose()
+        {
+            ParametrAsideChooseTFPurpose.Click();
+        }
+
+        public void OpenTFColorDdl()
+        {
+            ButtonAsideTFColor.Click();
+        }
+
+        public void ChooseTFColor()
+        {
+            ParametrAsideChooseTFColor.Click();
+        }
+
+        public void OpenTFSeasonalityDdl()
+        {
+            ButtonAsideTFSeasonality.Click();
+        }
+
+        public void ChooseTFSeasonality()
+        {
+            ParametrAsideChooseTFSeasonality.Click();
+        }
+
+        public void OpenTFSpecOEMyDdl()
+        {
+            ButtonAsideTFSpecOEM.Click();
+        }
+
+        public void ChooseTFSpecOEM()
+        {
+            ParametrAsideChooseTFSpecOEM.Click();
+        }
+
+        public void OpenTFScopeDdl()
+        {
+            ButtonAsideTFScope.Click();
+        }
+
+        public void ChooseTFScope()
+        {
+            ParametrAsideChooseTFScope.Click();
+        }
+    }
+}
+
