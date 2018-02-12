e445effe062d8ef4ed93ab52a7440a9ca809a55b
Aleksandr Rezenkov <aleksandr.rezenkov@sky-tech.co>
2018-02-12 16:50:16 +0200

    f

--- ToolsQA/TestsCases/BaseTest.cs
+++ ToolsQA/TestsCases/BaseTest.cs
@@ -1,10 +1,9 @@
 ﻿using NUnit.Framework;
-using NUnit.Framework.Interfaces;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
 using OpenQA.Selenium.Support.UI;
 using Protractor;
-using RelevantCodes.ExtentReports;
 using System;
 using ToolsQA.pages;
 
@@ -39,11 +38,18 @@ namespace ToolsQA.TestsCases
 
         public BaseTest()
         {
-            _driver = new ChromeDriver(System.Reflection.Assembly.GetExecutingAssembly().Location.Replace("ToolsQA.exe", ""));
+            // _driver = new ChromeDriver(System.Reflection.Assembly.GetExecutingAssembly().Location.Replace("ToolsQA.dll", ""));
+
+            var opts = new ChromeOptions();
+            opts.PlatformName = "Windows";
+
+            _driver = new RemoteWebDriver(new Uri("http://192.168.100.32:5555/wd/hub"), opts.ToCapabilities(), TimeSpan.FromMinutes(1));
             _driver.Manage().Timeouts().AsynchronousJavaScript = TimeSpan.FromSeconds(60);
+            _driver.Manage().Window.Maximize();
             _browser = new NgWebDriver(_driver);
             _browser.Manage().Window.Maximize();
             _browser.Url = Url + "/login";
+
         }
 
         [OneTimeSetUp]
